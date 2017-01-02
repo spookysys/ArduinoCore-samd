fcf1be0096fda1cbfce4f2181db2fc800642e164
Eivind Liland <spookysys@gmail.com>
2017-01-02 01:44:44 +0100

    Removed Serial - as this does not exist. Use SerialUSB instead.

--- variants/telemetry/variant.h
+++ variants/telemetry/variant.h
@@ -193,7 +193,7 @@ extern SERCOM sercom3;
 extern SERCOM sercom4;
 extern SERCOM sercom5;
 
-extern Uart Serial;
+//extern Uart Serial;
 extern Uart Serial1;
 
 #endif
@@ -214,7 +214,7 @@ extern Uart Serial1;
 // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
 //                            pins are NOT connected to anything by default.
 #define SERIAL_PORT_USBVIRTUAL      SerialUSB
-#define SERIAL_PORT_MONITOR         Serial
+#define SERIAL_PORT_MONITOR         SerialUSB
 // Serial has no physical pins broken out, so it's not listed as HARDWARE port
 #define SERIAL_PORT_HARDWARE        Serial1
 #define SERIAL_PORT_HARDWARE_OPEN   Serial1
