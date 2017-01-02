4d2013f4d086a23ab4bee97033083f851b7bbc97
Eivind Liland <spookysys@gmail.com>
2017-01-02 01:12:34 +0100

    Updated publishing script in "extras" like I want it

--- extras/pack.bash
+++ extras/pack.bash
@@ -17,35 +17,37 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-BUILD_NUMBER=$1
+# BUILD_NUMBER=$1
 CURR_TIME=`date "+%Y-%m-%d %H:%M"`
 CURR_TIME_SED=`date "+%Y\\-%m\\-%d %H:%M"`
-VERSION=9.9.9-Hourly
+VERSION=`grep version= platform.txt | sed 's/version=//g'`
 
 PWD=`pwd`
 FOLDERNAME=`basename $PWD`
 THIS_SCRIPT_NAME=`basename $0`
-FILENAME=package_samd-hourly-b${BUILD_NUMBER}.tar.bz2
+FILENAME=package_telemetry-${VERSION}.tar.bz2
 
 rm -f $FILENAME
 
 # Change name in platform.txt
-sed -i "s/name=.*/name=SAMD Hourly Build ${BUILD_NUMBER} (${CURR_TIME})/" platform.txt
+sed -i "s/name=.*/name=Telemetry-PCB (${CURR_TIME})/" platform.txt
 
 cd ..
-tar --transform "s|$FOLDERNAME|samd-hourly_b${BUILD_NUMBER}|g"  --exclude=extras/** --exclude=.git* --exclude=.idea -cjf $FILENAME $FOLDERNAME
+tar --transform "s|$FOLDERNAME|telemetry-${VERSION}|g"  --exclude=extras/** --exclude=publish/** --exclude=.git* --exclude=.idea -cjf $FILENAME $FOLDERNAME
 cd -
 
-mv ../$FILENAME .
+# restore platform.txt
+git checkout platform.txt
 
-CHKSUM=`sha256sum $FILENAME | awk '{ print $1 }'`
-SIZE=`wc -c $FILENAME | awk '{ print $1 }'`
+mv ../$FILENAME publish
 
-cat extras/package_index.json.Hourly.template |
-sed "s/%%BUILD_NUMBER%%/${BUILD_NUMBER}/" |
+CHKSUM=`sha256sum publish/$FILENAME | awk '{ print $1 }'`
+SIZE=`wc -c publish/$FILENAME | awk '{ print $1 }'`
+
+cat extras/package_index.json.template |
 sed "s/%%CURR_TIME%%/${CURR_TIME_SED}/" |
 sed "s/%%VERSION%%/${VERSION}/" |
 sed "s/%%FILENAME%%/${FILENAME}/" |
 sed "s/%%CHECKSUM%%/${CHKSUM}/" |
-sed "s/%%SIZE%%/${SIZE}/" > package_samd-hourly-build_index.json
+sed "s/%%SIZE%%/${SIZE}/" > publish/package_telemetry_index.json
 
